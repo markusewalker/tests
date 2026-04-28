0e965c4f04b9bfd8251ca7b0356d3d8c30673517
Jonathan Crowther <jonathan.crowther@suse.com>
2026-04-28 11:44:29 -0400

    Add HPA validation tests (#625)

--- actions/kubeapi/workloads/deployments/update.go
+++ actions/kubeapi/workloads/deployments/update.go
@@ -2,9 +2,12 @@ package deployments
 
 import (
 	"github.com/rancher/shepherd/clients/rancher"
+	"github.com/rancher/shepherd/extensions/defaults"
 	clusterapi "github.com/rancher/tests/actions/kubeapi/clusters"
 	appv1 "k8s.io/api/apps/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kwait "k8s.io/apimachinery/pkg/util/wait"
 )
 
 // UpdateDeployment is a helper function to update a deployment in a cluster. If waitForDeployment is true, the function will wait for the deployment to be active
@@ -14,18 +17,33 @@ func UpdateDeployment(client *rancher.Client, clusterID, namespaceName string, d
 		return nil, err
 	}
 
-	latestDeployment, err := wranglerContext.Apps.Deployment().Get(namespaceName, deployment.Name, metav1.GetOptions{})
-	if err != nil {
-		return nil, err
+	backoff := kwait.Backoff{
+		Duration: defaults.FiveSecondTimeout,
+		Factor:   1,
+		Jitter:   0,
+		Steps:    10,
 	}
 
-	deployment.ResourceVersion = latestDeployment.ResourceVersion
+	var updatedDeployment *appv1.Deployment
+	// Waiting for update to succeed
+	err = kwait.ExponentialBackoff(backoff, func() (finished bool, err error) {
+		latestDeployment, err := wranglerContext.Apps.Deployment().Get(namespaceName, deployment.Name, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
 
-	updatedDeployment, err := wranglerContext.Apps.Deployment().Update(deployment)
-	if err != nil {
-		return nil, err
-	}
+		deployment.ResourceVersion = latestDeployment.ResourceVersion
 
+		updatedDeployment, err = wranglerContext.Apps.Deployment().Update(deployment)
+		if err != nil {
+			if apierrors.IsConflict(err) {
+				// If there is a conflict, we need to get the latest version of the deployment and try again
+				return false, nil
+			}
+			return false, err
+		}
+		return true, nil
+	})
 	if waitForDeployment {
 		err = WaitForDeploymentActive(client, clusterID, namespaceName, deployment.Name)
 		if err != nil {
